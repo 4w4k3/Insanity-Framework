61de85230da403e834f8396fde54663f937aac97
Alisson Moretto <4w4k3@protonmail.com>
2017-02-02 16:37:31 -0200

    Update checker.py

--- checker.py
+++ checker.py
@@ -2,6 +2,8 @@
 # Written by: * Alisson Moretto - 4w4k3
 # https://github.com/4w4k3/Insanity-Framework
 # Licensed under the BSD-3-Clause
+
+import urllib2
 import os
 import sys
 BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
@@ -28,7 +30,23 @@ os.system('wine pywin32.exe')
 os.system('wine /root/.wine/drive_c/Python27/python.exe -m pip install pyinstaller')
 os.system('wine /root/.wine/drive_c/Python27/python.exe -m pip uninstall Crypto')
 os.system('wine /root/.wine/drive_c/Python27/python.exe -m pip uninstall pycrypto')
-os.system('wine msiexec /i VCForPython27.msi /L*v log.txt')
+os.system('clear')
+print '\n'
+print ' ################################################################## '
+print '\n'
+print '\n'
+print '             *{0} DOWNLOADING VCFORPYTHON2.7.msi {1}* '.format(GREEN, END)
+print '                          - PLEASE WAIT -'
+print '\n'
+print '\n'
+print ' ################################################################## '
+def one():
+	response = urllib2.urlopen('https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi')
+	data = response.read()
+	fileup = open("VCForPython27.msi", 'w')
+	fileup.write(data)
+	fileup.close()
+one()
+os.system('wine msiexec /i VCForPython27.msi /L*v log2.txt')
 os.system('wine /root/.wine/drive_c/Python27/python.exe -m pip install pycrypto')
 os.system('mkdir .OK')
-
